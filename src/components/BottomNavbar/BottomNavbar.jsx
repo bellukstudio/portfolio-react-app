9e519c2aa465ffe8ff61bd94b9f73c6fc07756ab
bellukchips <bellukchips@gmail.com>
2025-01-09 15:10:06 +0700

    HOTFIX BOTTOM NAVBAR

--- src/components/BottomNavbar/BottomNavbar.jsx
+++ src/components/BottomNavbar/BottomNavbar.jsx
@@ -4,9 +4,9 @@ export default function BottomNavbar() {
     <div>
       <div className="fixed px-10 z-50 w-[340px] h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600 md:hidden">
         <div className="grid h-full max-w-lg grid-cols-6 mx-auto">
-          <button
+          <a
+            href="#home"
             data-tooltip-target="tooltip-home"
-            type="button"
             className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
@@ -19,7 +19,7 @@ export default function BottomNavbar() {
               <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
             </svg>
             <span className="sr-only">Home</span>
-          </button>
+          </a>
           <div
             id="tooltip-home"
             role="tooltip"
@@ -29,9 +29,9 @@ export default function BottomNavbar() {
             <div className="tooltip-arrow" data-popper-arrow></div>
           </div>
 
-          <button
+          <a
+            href="#profile"
             data-tooltip-target="tooltip-profile"
-            type="button"
             className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
@@ -44,7 +44,7 @@ export default function BottomNavbar() {
               <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
             </svg>
             <span className="sr-only">Profile</span>
-          </button>
+          </a>
           <div
             id="tooltip-profile"
             role="tooltip"
@@ -54,9 +54,9 @@ export default function BottomNavbar() {
             <div className="tooltip-arrow" data-popper-arrow></div>
           </div>
 
-          <button
+          <a
             data-tooltip-target="tooltip-experience"
-            type="button"
+            href="#experience"
             className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
@@ -74,7 +74,7 @@ export default function BottomNavbar() {
             </svg>
 
             <span className="sr-only">Experience</span>
-          </button>
+          </a>
           <div
             id="tooltip-experience"
             role="tooltip"
@@ -84,9 +84,9 @@ export default function BottomNavbar() {
             <div className="tooltip-arrow" data-popper-arrow></div>
           </div>
 
-          <button
+          <a
             data-tooltip-target="tooltip-skill"
-            type="button"
+            href="#skills"
             className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
@@ -102,7 +102,7 @@ export default function BottomNavbar() {
               />
             </svg>
             <span className="sr-only">Skill</span>
-          </button>
+          </a>
 
           <div
             id="tooltip-skill"
@@ -113,9 +113,9 @@ export default function BottomNavbar() {
             <div className="tooltip-arrow" data-popper-arrow></div>
           </div>
 
-          <button
+          <a
             data-tooltip-target="tooltip-skill"
-            type="button"
+            href="#portfolio"
             className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
@@ -131,7 +131,7 @@ export default function BottomNavbar() {
               />
             </svg>
             <span className="sr-only">Portfolio</span>
-          </button>
+          </a>
           <div
             id="tooltip-portfolio"
             role="tooltip"
@@ -141,9 +141,9 @@ export default function BottomNavbar() {
             <div className="tooltip-arrow" data-popper-arrow></div>
           </div>
 
-          <button
+          <a
             data-tooltip-target="tooltip-contact"
-            type="button"
+            href="#contact"
             className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
@@ -162,7 +162,7 @@ export default function BottomNavbar() {
             </svg>
 
             <span className="sr-only">Contact</span>
-          </button>
+          </a>
           <div
             id="tooltip-contact"
             role="tooltip"
